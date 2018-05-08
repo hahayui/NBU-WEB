8b3c9d00ee8b6880dc0f9546cff047d018788e29
hahayui <eliankurtev@gmail.com>
2018-05-08 18:31:43 +0300

    Fixed js index file

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     var srcImg;
-    var btcTmp = 0;
-    var highestPriceTMP = 0;
-    var lowestPriceTMP = 0;
+    var btcTmp = 0.;
+    var valueFinal = 0.;
+    var highestPriceTMP = 0.;
+    var lowestPriceTMP = 0.;
     //var btcTmpForSecondReload = 0;
-    console.log(btcTmp);
     /*FUNCTION THAT SHOWS THE INFORMATION IN THE INDEX.HTML FILE ABOUT BTCS RATE*/
     function print(stoinost, imgSrc, highestPriceFTD, lowestPriceFTD) {
         $("#p p").text("1 BTC = " + valueFinal);
@@ -15,37 +15,7 @@ $(document).ready(function() {
 
     /*FUNCTION THAT LOADS FROM EXTERNAL JSON FILE THE INFO ABOUT THE RATE OF BTC IN BGN FOR FIRST TIME. 
         15 SECS AFTER THE "function reloadSecondURL" LOADS*/
-    var valueFinal = 0.;
 
-    function reload() {
-        $.get('https://www.bitstamp.net/api/v2/ticker_hour/btceur/', function(data, status) {
-            var fixBNB = 1.95583; /*EUR IN BGN - INFO FROM BNB*/
-            console.log('Status:', status);
-            console.log('data:', data);
-            var btc = data.last; /*GETTING THE LAST VALUE FOR 1BTC*/
-            btcTmp = btc; /* TRANSFERING THE VALUE OF 1BTC TO TEMPORARY VARIABLE TO MAKE COMAPRISOON BETWEEN THEM*/
-            //console.log(Number(btcTmp));
-            if (btcTmp != btc) { /*DOING A COMAPRISON BETWEEN THE 2 VARIABLES AND TAKE THE CORRESPONDING RESULT*/
-                //console.log(btcTmp);
-                var btcInBGN = btcTmp * fixBNB;
-                //console.log(btcInBGN);
-            } else {
-                //console.log(btc);
-                var btcInBGN = btc * fixBNB;
-                //console.log(btcInBGN);
-            }
-            var highestPrice = data.high;
-            highestPriceTMP = ((highestPrice) * fixBNB).toFixed(2) + " BGN ";
-            var lowestPrice = data.low;
-            lowestPriceTMP = ((lowestPrice) * fixBNB).toFixed(2) + " BGN ";
-            valueFinal = ((btcInBGN).toFixed(2)) + " BGN ";
-            //console.log(valueFinal);
-            srcImg = './images/minus.png';
-            print(valueFinal, srcImg, highestPriceTMP, lowestPriceTMP);
-
-        });
-    }; /*CLOSING "reload" function*/
-    reload();
 
     /*FUNCTION THAT SHOWS THE LAST RATE INFO DOR BTC ON EVERY 15 SECS*/
     function reloadSecondURL() {
@@ -76,31 +46,34 @@ $(document).ready(function() {
             if (btc < btcTmp) {
                 //console.log(btc < btcTmp);
                 srcImg = './images/down-arrow.png';
+            } else if (btcTmp == 0) {
+                srcImg = './images/minus.png';
             } else if (btc > btcTmp) {
                 srcImg = './images/up-arrow.png';
 
             } else if (btc == btcTmp) {
                 srcImg = './images/minus.png';
             }
+
             var highestPrice = data.high;
             highestPriceTMP = ((highestPrice) * fixBNB).toFixed(2) + " BGN ";
             var lowestPrice = data.low;
             lowestPriceTMP = ((lowestPrice) * fixBNB).toFixed(2) + " BGN ";
-            valueFinal = ((btcInBGN).toFixed(2)) + " BGN ";
 
             valueFinal = ((btcInBGN).toFixed(2)) + " BGN ";
 
             print(valueFinal, srcImg, highestPriceTMP, lowestPriceTMP);
+
             btcTmp = btcTmp1;
             btcTmp1 = 0;
             //console.log(valueFinal);
             //print(valueFinal, srcImg);
-
+            setTimeout(reloadSecondURL, 15000); //RELOADING ON EVERY 15 SEC THE FUNCTION WITH UPDATED DATA 
         });
     }; //CLOSING reloadSecondURL();
-
-    setInterval(reloadSecondURL, 15000); //RELOADING ON EVERY 15 SEC THE FUNCTION WITH UPDATED DATA 
-
+    //reloadSecondURL();
+    //setInterval(reloadSecondURL, 15000); //RELOADING ON EVERY 15 SEC THE FUNCTION WITH UPDATED DATA 
+    reloadSecondURL();
 
     //RESPONSIVE FIRST AND SECOND ROW
     var h = $(window).height() - 114; //+secondRow height
